50d5a0610454b7c34da3b3c550fe1df5814d7c88
iiyor <mfahryfadil@gmail.com>
2026-07-17 05:15:59 +0700

    log mongo url
    Log the MongoDB URL from the environment variable.

--- src/main/java/com/Project/backend/Chap_App_Backend/ChapAppBackendApplication.java
+++ src/main/java/com/Project/backend/Chap_App_Backend/ChapAppBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChapAppBackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("mongo url: " + System.getenv("MONGO_URL"));
 		SpringApplication.run(ChapAppBackendApplication.class, args);
 	}
 
