cddf2cf0c61f3f0c5348e63597663cb1d1001c6b
iiyor <mfahryfadil@gmail.com>
2026-07-17 08:27:11 +0700

    remove mongo url log debug

--- src/main/java/com/Project/backend/Chap_App_Backend/ChapAppBackendApplication.java
+++ src/main/java/com/Project/backend/Chap_App_Backend/ChapAppBackendApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChapAppBackendApplication {
 
 	public static void main(String[] args) {
-		System.out.println("mongo url: " + System.getenv("MONGO_URL"));
 		SpringApplication.run(ChapAppBackendApplication.class, args);
 	}
 
